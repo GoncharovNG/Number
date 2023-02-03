e68038ca3daecfc365b3f500374d5ff2bf020e4c
GoncharovNG <123543902+GoncharovNG@users.noreply.github.com>
2023-02-03 16:13:54 +0300

    Задание спринт 3

--- Number/ViewController.swift
+++ Number/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var userScore: Int = 0
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var titleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textLabel.text = "\(userScore)"
+        textLabel.tintColor = .green // цвет
+        titleButton.backgroundColor = UIColor.purple
     }
-
+    func updateTextLabels () {
+        textLabel.text = "\(userScore)"
+        userScore += 1
+    }
+    @IBAction func buttonDidTap(_ sender: Any) {
+        updateTextLabels()
+        print("Нажатие")
+    }
 
 }
 
